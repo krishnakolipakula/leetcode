a6ed74e74d91bfdf2a60627767eeb35c604f73ea
krishna8757 <105148224+krishna8757@users.noreply.github.com>
2023-11-18 23:01:18 +0530

    Time: 41 ms (74.20%), Space: 16.4 MB (29.39%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        left, right = 1, x
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
